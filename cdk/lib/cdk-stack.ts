71890748b2c1378a77b051ed926867b966df9de8
Takuya Mukohira <takuya.mk96@gmail.com>
2022-11-23 20:48:26 +0900

    Create empty statemachine

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,6 +4,7 @@ import { StackProps } from './stack-props';
 
 import { buildS3Bucket } from './resources/crawler/s3';
 import { buildFetchNewsLambda, buildRuleForFetchNewsLambda } from './resources/crawler/fetchNewsLambda';
+import { buildCrawlerStateMachine, buildRuleForCrawlerStateMachine } from './resources/crawler/crawler';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -13,5 +14,8 @@ export class CdkStack extends cdk.Stack {
     const fetchNewsLambda = buildFetchNewsLambda(this, props);
     s3Bucket.grantReadWrite(fetchNewsLambda);
     buildRuleForFetchNewsLambda(this, props, fetchNewsLambda);
+
+    const stateMachine = buildCrawlerStateMachine(this, props, s3Bucket);
+    buildRuleForCrawlerStateMachine(this, props, stateMachine, s3Bucket);
   }
 }
---
+++ cdk/lib/resources/crawler/crawler.ts
@@ -0,0 +1,58 @@
+import {
+  aws_iam as iam,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks,
+  RemovalPolicy,
+  aws_logs as logs,
+  aws_s3 as s3,
+  aws_events as events,
+  aws_events_targets as targets,
+  Duration,
+} from 'aws-cdk-lib';
+import { CdkStack } from '../../cdk-stack';
+import { StackProps } from '../../stack-props';
+
+export const buildCrawlerStateMachine = (stack: CdkStack, props: StackProps, bucket: s3.Bucket) => {
+  const stepfunctionsLogGroupName = `/aws/vendedlogs/states/tmnct-news-crawler-${props.stage}`;
+  const stepFunctionsLogGroup = new logs.LogGroup(stack, `stepfunctions-log-group-${props.stage}`, {
+    logGroupName: stepfunctionsLogGroupName,
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
+
+  const definition = new sfn.Pass(stack, 'PASS');
+
+  const stateMachine = new sfn.StateMachine(stack, `crawlerStateMachine-${props.stage}`, {
+    definition,
+    logs: {
+      destination: stepFunctionsLogGroup,
+    },
+    timeout: Duration.minutes(5),
+  });
+
+  return stateMachine;
+};
+
+export const buildRuleForCrawlerStateMachine = (
+  stack: CdkStack,
+  props: StackProps,
+  stateMachine: sfn.StateMachine,
+  bucket: s3.Bucket
+) => {
+  return new events.Rule(stack, `ruleForCrawlerStateMachine-${props.stage}`, {
+    eventPattern: {
+      source: ['aws.s3'],
+      detailType: ['Object Created'],
+      resources: [bucket.bucketArn],
+    },
+    targets: [
+      new targets.SfnStateMachine(stateMachine, {
+        input: events.RuleTargetInput.fromObject({
+          object: {
+            bucketName: events.EventField.fromPath('$.detail.bucket.name'),
+            key: events.EventField.fromPath('$.detail.object.key'),
+          },
+        }),
+      }),
+    ],
+  });
+};
