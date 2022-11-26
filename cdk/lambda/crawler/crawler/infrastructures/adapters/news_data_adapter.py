f752559768a79007ce8efaf016ea701334f2249c
Takuya Mukohira <takuya.mk96@gmail.com>
2022-11-26 16:41:36 +0900

    Impl InputDataAdapter / NewsDataAdapter

---
+++ cdk/lambda/crawler/crawler/infrastructures/adapters/news_data_adapter.py
@@ -0,0 +1,18 @@
+import json
+from dateutil import parser
+
+from crawler.domains.adapters import InputDataAdapter
+from crawler.models import News
+
+
+class NewsDataAdapter(InputDataAdapter):
+    def transform(self, data: dict) -> News:
+        return News(
+            title=data["title"],
+            wp_pid=data["wp_pid"],
+            url=data["url"],
+            pub_date=parser.parse(data["pub_date"]).date(),
+            category=data["category"],
+            content=data["content"],
+            slug=data["slug"],
+        )
